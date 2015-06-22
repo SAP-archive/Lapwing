2f41188d5767148c61b7004f28a37d39e3ca8975
i044892 <vincenzo.turco@sap.com>
2015-06-22 19:27:03 +0200

    removed unnecessary dependency

--- okapi-application-lapwing/src/test/java/net/sf/okapi/applications/lapwing/test/PipelineTest.java
+++ okapi-application-lapwing/src/test/java/net/sf/okapi/applications/lapwing/test/PipelineTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
